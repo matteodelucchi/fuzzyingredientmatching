d11a0bd85b5bfa29ede9274b79d4dc91054bd4af
matteodelucchi <mail@matteodelucchi.com>
2019-11-12 16:52:46 -0300

    fixed typos and pathways

--- merge_all.py
+++ merge_all.py
@@ -7,7 +7,7 @@ def load_dataframe(LUT, variables):
 
     Args: 
         LUT         str. Filepath to the hoga-bls or pks-bls look up table which results from fuzzyingredientmatching*.py
-        variables   list of strings with the column names to keep. Basicalls just the id and the correstponding product name.
+        variables   list of strings with the column names to keep. Basically just the id and the correstponding product name.
 
     Return:
         pandas data frame with the necessary variables.
@@ -17,11 +17,11 @@ def load_dataframe(LUT, variables):
 
 #----------------------------------------
 ### Load the two individal matchings as pandas data frames
-hoga_bls = load_dataframe("LUT_hoga_BLS.csv", ['idHogaProd', 'Hogaprodukt', 'BLS_Code', 'BLS_Bezeichnung'])
-pks_bls = load_dataframe("PKS_BLS_final_20190924.csv", ['idPauliProd', 'Pauliprodukt', 'BLS_Code', 'BLS_Bezeichnung'])
+hoga_bls = load_dataframe("/home/delt/ZHAW/MNIdatabase/LUT_hoga_BLS_Marleen-BB_empty-cells.csv", ['idHogaProd', 'Hogaprodukt', 'BLS_Code', 'BLS_Bezeichnung'])
+pks_bls = load_dataframe("/home/delt/ZHAW/MNIdatabase/PKS_BLS_final_20190924.csv", ['idPauliProd', 'Pauliprodukt', 'BLS_Code', 'BLS_Bezeichnung'])
 
 ### Merge the two matchings by their BLS_Code (serves as key)
 pks_bls_hoga = pd.merge(pks_bls, hoga_bls, on=['BLS_Code'], how='inner')
 
 ### Save the merged Look-up-table
-pks_bls_hoga.to_csv(r'LUT_PKS_HOGA_BLS_part_corr.csv')
+pks_bls_hoga.to_csv(r'LUT_PKS_HOGAlimmat_BLS_corr_BB.csv')
